8be856bc6e944d305ac40288001799b49b5ab88a
YoshinoriHashizume <yoshinori_hashizume@marueikogyo.jp>
2025-06-11 13:39:17 +0900

    完成

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by Yoshinori Hashizume on 2025/06/09.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
